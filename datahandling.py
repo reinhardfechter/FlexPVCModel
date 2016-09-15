ecb2cfcf36c8034261035fb321fae993c995d3d8
reinhardfechter <reinhard.fechter@gmail.com>
2016-09-15 17:20:04 +0200

    Finished thermomat single value analysis, started rheomix sva

--- datahandling.py
+++ datahandling.py
@@ -2,15 +2,16 @@ import json
 import os
 import glob
 import pandas as pd
+from tinydb import Query, TinyDB
 
 # read configuration file and expand ~ to user directory
 
 def alldatafiles(equipment):
     with open('config.json') as f:
         config = json.load(f)
-        datadir = os.path.expanduser(config[equipment])
+        datadir = os.path.expanduser(config['Raw_Data'] + equipment + '/')
     
-	if equipment == 'thermomat':
+	if equipment == 'thermomat' or equipment == 'rheomix':
 	    file_type = '*.txt'
 	else:
 	    file_type = '*.csv'
@@ -26,6 +27,8 @@ class DataFile:
             self.data = pd.read_table(filename, skiprows = 4, sep=';')
         elif equipment == 'ConeCal':
             self.data = pd.read_table(filename, skiprows = 1, sep=',')
+        elif equipment == 'rheomix':
+            self.data = pd.read_table(filename, skiprows = 3, sep=';')
 
     def simple_data(self, equipment):
         if equipment == 'thermomat':
@@ -33,10 +36,14 @@ class DataFile:
             time_data = time_data/60
             conduct_data = self.data[self.data.columns[1]].values
             data = [time_data, conduct_data]
-        if equipment == 'ConeCal':
+        elif equipment == 'ConeCal':
             time_data = self.data['Time (s)'].values
             HRR_data = self.data[self.data.columns[3]].values
             data = [time_data, HRR_data]
+        elif equipment == 'rheomix':
+            time_data = self.data['t [min]'].values
+            torque_data = self.data['Torque [Nm]'].values
+            data = [time_data, torque_data]
 			
         return data
 
@@ -52,7 +59,34 @@ def file_parse(f, equipment):
         split_filename = filename.split(' ')
         split_filename = split_filename[1].split('_')
         sample_number = split_filename[0]
+    elif equipment == 'rheomix':
+        split_filename = filename.split('_')
+        sample_number = split_filename[1][1:]
     
     return sample_number
-	
 
+def my_query(db, equipment, sample_number, data_type):
+    my_q = ((Query().equipment_name == equipment) &
+            (Query().sample_number == int(sample_number)) &
+            (Query().data_type == data_type))
+    return my_q
+
+def insert_update_db(db, update, equipment, sample_number, names, values):
+    for n, v in zip(names, values):
+        if update == False:
+            entry = {'equipment_name': equipment,
+                     'sample_number': int(sample_number),
+                     'data_type': n,
+                     'value': v
+                    }
+            db.insert(entry)
+
+        elif update == True:
+            db.update({'value': v}, my_query(db, equipment, sample_number, n))
+
+def access_db():
+    with open('config.json') as f:
+        config = json.load(f)
+        path = config['Results_Database']
+    my_db = TinyDB(path + 'Single_Value_Database.json')
+    return my_db
--- thermomat_single_val_anal.py
+++ thermomat_single_val_anal.py
@@ -1,4 +1,4 @@
-from datahandling import alldatafiles, DataFile, file_parse
+from datahandling import alldatafiles, DataFile, file_parse, insert_update_db, my_query
 from time import time as tm
 import pandas as pd
 from matplotlib.backends.backend_pdf import PdfPages
@@ -9,89 +9,85 @@ from numpy import trapz
 import matplotlib.pyplot as plt
 from tinydb import Query
 
-def thermomat_sva(db):
-    equipment = 'thermomat'
+def thermomat_sva(db, redo):
+    # Setting redo = True will repeat analysis even if it has been done for that sample already
+    # The data in the data base will only be updated if the Error is less
     
+    equipment = 'thermomat'
+
     Files = alldatafiles(equipment)
-	
+    
     t = tm()
 
     Q = Query()
 
-    with PdfPages('Test.pdf') as pdf:
-        for j, f in enumerate(Files[:3]):
-			split_tm = tm ()
-			
-			# Parsing filename
-			sample_number = file_parse(f, equipment)
-			
-			# Check if the relevant data exists and only do fit if necessary
-			check = db.search((Q.equipment_name == 'thermomat') & (Q.sample_number == int(sample_number)))
-			if len(check) == 0:
-			
-				# Get data
-				time_data, conduct_data = DataFile(f, equipment).simple_data(equipment)
+   
+    for j, f in enumerate(Files[:1]):
+        split_tm = tm ()
+        
+        # Parsing filename
+        sample_number = file_parse(f, equipment)
+        
+        # Check if the relevant data exists and only do fit if necessary
+        check = db.search((Q.equipment_name == 'thermomat') & (Q.sample_number == int(sample_number)))
+        if len(check) == 0 or redo == True:
+            # Get data
+            time_data, conduct_data = DataFile(f, equipment).simple_data(equipment)
 
-				# Trim Data
-				cut_point = find_cut_point(conduct_data)
+            # Trim Data
+            cut_point = find_cut_point(conduct_data)
 
-				if cut_point != None:
-					time_data = time_data[:cut_point]
-					conduct_data = conduct_data[:cut_point]
+            if cut_point != None:
+                time_data = time_data[:cut_point]
+                conduct_data = conduct_data[:cut_point]
 
-				# Remove offset
-				min_cond = min(conduct_data)
-				conduct_data[:] = [cond - min_cond for cond in conduct_data]        
+            # Remove offset
+            min_cond = min(conduct_data)
+            conduct_data[:] = [cond - min_cond for cond in conduct_data]        
 
-				# Find max conductivity to set upper limit of initial beta value
-				max_cond = max(conduct_data)
-				ini_val_up_lim = [50.0, 500.0, 3.0*max_cond, 1.5]
+            # Find max conductivity to set upper limit of initial beta value
+            max_cond = max(conduct_data)
+            ini_val_up_lim = [50.0, 500.0, 3.0*max_cond, 1.5]
 
-				# Fit Data with multiple starts 
-				starts = 10
-				smallest_err = 10000000.0
+            # Fit Data with multiple starts 
+            starts = 10
+            smallest_err = 10000000.0
 
-				for i in range(starts):
-					ini_val = rand_ini_val(ini_val_up_lim)
-					p = parameters(ini_val, ini_val_up_lim)
-					result = minimize(f2min, p, args=(time_data, conduct_data))
+            for i in range(starts):
+                ini_val = rand_ini_val(ini_val_up_lim)
+                p = parameters(ini_val, ini_val_up_lim)
+                result = minimize(f2min, p, args=(time_data, conduct_data))
 
-					err_list = f2min(p, time_data, conduct_data)
-					abs_err_list = map(abs, err_list)
-					int_abs_err = trapz(abs_err_list, x=time_data)
+                err_list = f2min(p, time_data, conduct_data)
+                abs_err_list = map(abs, err_list)
+                int_abs_err = trapz(abs_err_list, x=time_data)
 
-					smallest_err = min([smallest_err, int_abs_err])
-					if smallest_err == int_abs_err:
-						best_p = p
+                smallest_err = min([smallest_err, int_abs_err])
+                if smallest_err == int_abs_err:
+                    best_p = p
+  
+            # Entering info into tinydb
+            # If previous value exist only update if err is less
+            p_names = ['theta', 'tau', 'beta', 'm']
+            values = [best_p[i].value for i in p_names]
+            
+            data_types = p_names
+            data_types.append('int_of_abs_err')
+            values.append(smallest_err)
+            
+            if len(check) == 0:
+                insert_update_db(db, False, equipment, sample_number, data_types, values)
+            else:
+                old_err = db.search(my_query(db, equipment, sample_number, 'int_of_abs_err'))[0]['value']
+                if smallest_err < old_err:
+                    insert_update_db(db, True, equipment, sample_number, data_types, values)
+                    print 'Updated Sample Number', sample_number
+                              
+            split_tm = tm() - split_tm
+            print 'Completed Fit', (j + 1), 'in', round(split_tm, 2), '(s)'
+        else:
+            print 'Skipped Fit', (j + 1)
 
-				# Plot fitted model and raw data
-				fig = plt.figure()
-				fig.suptitle('Sample Number ' + sample_number)
-				plt.plot(time_data, conduct_data)
-				plt.plot(time_data, cond_model(best_p, time_data))
-
-				pdf.savefig(fig)
-				
-				# Entering info into tinydb
-				p_names = ['theta', 'tau', 'beta', 'm']
-
-				for pn in p_names:
-					entry = {'equipment_name': 'thermomat', 
-							 'sample_number': int(sample_number),
-							 'data_type': pn,
-							 'value': best_p[pn].value}
-					db.insert(entry)
-
-				db.insert({'equipment_name': 'thermomat', 
-						   'sample_number': int(sample_number),
-						   'data_type': 'int_of_abs_err',
-						   'value': smallest_err})
-				
-				split_tm = tm() - split_tm
-				print 'Completed Fit', (j + 1), 'in', round(split_tm, 2), '(s)'
-			else:
-				print 'Skipped Fit', (j + 1)
-
-	req_time = tm() - t
-	print '******************'
-	print 'Time required (min) =', round(req_time/60.0, 2)
+    req_time = tm() - t
+    print '******************'
+    print 'Time required (min) =', round(req_time/60.0, 2)
